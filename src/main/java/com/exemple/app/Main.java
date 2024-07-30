b958ea331366752a375194e68197ec47a6e6dfbe
bezborodkoS <rotp25@gmail.com>
2024-07-30 18:53:16 +0300

    first comit

---
+++ src/main/java/com/exemple/app/Main.java
@@ -0,0 +1,9 @@
+package com.exemple.app;
+
+import javax.crypto.spec.PSource;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First Start");
+    }
+}
